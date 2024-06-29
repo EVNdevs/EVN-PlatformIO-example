7fb2a305ad5cb1d9dcedfbc4a898d7b1fb19428a
HTY2003 <randumbperson@gmail.com>
2024-06-30 06:01:59 +0800

    Add all necessary files for compilation

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include <EVN.h>
+
+//by default, the board is set such that the button toggles on/off with each press, and the LED output is linked to the button
+EVNAlpha board;
+
+void setup() {
+  board.begin();  //initialize board at start of void setup()
+}
+
+void loop() {
+  //nothing here, but pressing the button should toggle the LED on and off
+}
